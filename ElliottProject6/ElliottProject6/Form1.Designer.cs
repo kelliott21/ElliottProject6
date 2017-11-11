d057f9c1fd7051c7efb32942141035b8731c2450
kelliott21 <31453577+kelliott21@users.noreply.github.com>
2017-11-11 13:53:25 -0600

    Add files via upload

---
+++ ElliottProject6/ElliottProject6/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ElliottProject6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(10, 42);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(64, 13);
+            this.lblPrompt.TabIndex = 0;
+            this.lblPrompt.Text = "Enter Word:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(83, 39);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(205, 20);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(213, 75);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(213, 116);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 85);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(35, 13);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "label1";
+            this.lblResult.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 151);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblPrompt);
+            this.Name = "Form1";
+            this.Text = "Hash Table Word Search";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ ElliottProject6/ElliottProject6/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElliottProject6
+{
+    public partial class Form1 : Form
+    {
+        private Hashtable hTable = new Hashtable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string text = System.IO.File.ReadAllText(@"BinaryTextFile.txt");
+
+            //Splits text file by the characters below
+            char[] delimiterChars = { ' ', '"', '.', ':', '\t', '\r', '\n', '?', '!', ';', '(', ')' };
+            string[] words = text.Split(delimiterChars);
+
+            int index = 0;
+            foreach (string s in words)
+            {
+                if (!string.IsNullOrWhiteSpace(s.Replace(",", "")))
+                {
+                    //ToLower makes all words lowercase
+                    hTable.Add(index,s.ToLower().Replace(",", ""));
+                    index++;
+                }
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            lblResult.Visible = false;
+            if(string.IsNullOrWhiteSpace(txtInput.Text))
+            {
+                MessageBox.Show("A value is required.");
+                return;
+            }
+            bool resultFound = hTable.ContainsValue(txtInput.Text.ToLower());
+            if(resultFound)
+            {
+                lblResult.Text = "Value was found in the hash table.";
+                lblResult.Visible = true;
+            }
+            else
+            {
+                lblResult.Text = "Value was NOT found in the hash table.";
+                lblResult.Visible = true;
+            }
+        }
+
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                btnSearch_Click(sender, e);
+            }
+        }
+    }
+}
